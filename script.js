1d35a20e2a5cc7d1b6b4229104ea4a74e890da40
Krystel <90385292+Alyssa0114@users.noreply.github.com>
2024-12-31 15:40:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//localStorage.setItem ("clé","valeur")
+//localStorage.getItem ("clé")
+//localStorage.clear();
+
+if(localStorage.getItem("nom") != null)
+    h1.textContent = `Bonjour ${localStorage.getItem("nom")}`;
+
+bouton.onclick = () => {
+    localStorage.setItem("nom",nom.value);
+}
+
